46dda7c09273ad4f8d5f3ccb31a377171ec95e42
Амир Хасанов <aworldx@gmail.com>
2018-01-04 16:13:42 +0300

    using seed and nokogiri in fourth task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open("http://1000mostcommonwords.com/1000-most-common-german-words/"))
+doc.xpath('//article[@id="post-188"]//table//tr/td').drop(3).each_slice(3) do |tr|
+  card = Card.new(:original_text => tr[1].text, :translated_text => tr[2].text)
+  card.save
+end
