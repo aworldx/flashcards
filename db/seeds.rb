40bc69976214fd52c3345017f6b2f92f0e063997
Амир Хасанов <aworldx@gmail.com>
2018-01-04 16:27:36 +0300

    1.9 hash syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://1000mostcommonwords.com/1000-most-common-german-words/"))
 doc.xpath('//article[@id="post-188"]//table//tr/td').drop(3).each_slice(3) do |tr|
-  card = Card.new(:original_text => tr[1].text, :translated_text => tr[2].text)
+  card = Card.new(original_text: tr[1].text, translated_text: tr[2].text)
   card.save
 end
