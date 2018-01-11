13ca00d8ed6941e1d0cf213eab9d1060f4ee0dd1
Khasanov Amir <aworldx@gmail.com>
2018-01-11 14:14:57 +0000

    fix error in spec helper

--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,5 +1,5 @@
 module AuthenticationForFeatureRequest
-  def login (user, password = 'vrdffpswrd')
+  def login(user, password = 'vrdffpswrd')
     user.update_attributes password: password
 
     page.driver.post user_sessions_path, { user: { email: user.email, password: password } }
