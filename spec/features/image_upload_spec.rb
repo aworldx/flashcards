16e0ed68eaee6c0d504906b587f0ffef9f735de1
Khasanov Amir <aworldx@gmail.com>
2018-01-16 12:31:55 +0000

    tests

--- spec/features/image_upload_spec.rb
+++ spec/features/image_upload_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'upload an image', type: :feature do
-  let!(:pass) { 'pass' }
+  let(:pass) { 'pass' }
   let!(:user) { create(:user, password: pass, password_confirmation: pass) }
 
-  context 'when user logged in and add new card' do
+  context 'when user logged in and visit edit card page' do
     
     before(:each) do
       visit login_path
@@ -13,33 +13,33 @@ describe 'upload an image', type: :feature do
       
       click_button 'Вход'
 
-      visit root_path
-      click_link 'Добавить карточку'
-      
-      fill_in :card_original_text, with: 'new card'
-      fill_in :card_translated_text, with: 'новая карточка'
-
-      page.attach_file(:card_avatar, 'spec/support/help.png')
-
-      click_button 'Сохранить'
+      visit "cards/#{user.cards.find_by_original_text('hello world').id}/edit"
     end
 
-    it "observes notice 'Новая карточка добавлена'" do
-      expect(page).to have_content 'Новая карточка добавлена'
+    it "should not observe card's image" do
+      expect(page).not_to have_xpath("//img[@id='avatar']")
     end
 
-    it "card model attr contains avatar" do
-      card = Card.find_by_original_text('new card')
-      expect(card.avatar_file_name).to eq('help.png')
+    context 'after user upload image' do
+      before(:each) do
+        page.attach_file(:card_avatar, 'spec/support/help.png')
+        click_button 'Сохранить'
+      end
+
+      it 'user should observe card image on edit page' do
+        expect(page).to have_xpath("//img[@id='avatar']")
+      end
+
+      it 'model should contains image' do
+        card = Card.find_by_original_text('hello world')
+        expect(card.avatar_file_name).to eq('help.png')
+      end
     end
 
     it "root url contains avatar" do
-      card = Card.find_by_original_text('hello world')
-      card.destroy
-
       Timecop.freeze(Date.today + 3) do
         visit root_path
-        expect(page).to have_css("img[src*='help.png']")
+        expect(page).to have_xpath("//img[@id='avatar']")
       end
     end
   end
