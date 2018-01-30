91a1273b038d771ecc03672404dea85c572624a8
Khasanov Amir <aworldx@gmail.com>
2018-01-30 08:39:50 +0000

    migration for remove current attr from decks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129112336) do
+ActiveRecord::Schema.define(version: 20180130083714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20180129112336) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "current"
     t.index ["user_id"], name: "index_decks_on_user_id"
   end
 
