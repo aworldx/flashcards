49004ff882d909f5809e55563519f109d933403d
vagrant <vagrant@railsbox>
2018-02-20 22:16:04 +0000

    nineteenth-task

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
     scope = current_user.current_deck.present? ? current_user.current_deck : current_user
     @card = scope.cards.unreviewed.first
     @misprint = params[:misprint]
+
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'form', layout: false }
+    end
   end
 end
--- app/views/cards/check_translate.js.coffee
+++ app/views/cards/check_translate.js.coffee
@@ -1,3 +1,8 @@
 $ ->
-  $('#form_section').empty()
-  .append("<%= j render 'home/form', locals: { card: nil } %>")
+  $.ajax "<%= root_path %>",
+    dataType: 'script',
+    error: (jqXHR, textStatus, errorThrown) ->
+      alert textStatus
+    success: (data, textStatus, jqXHR) ->
+      console.log(data)
+      $('#form_section').empty().append(data)
